80bef27ad7f71f32dd71e4a0d5bf023752e3f75a
薛聿明 <102m05008@stud.sju.edu.tw>
2013-12-18 22:20:58 +0800

    use config file.

--- src/edu/sju/ee98/daq/core/frequency/response/FrequencyResponse.java
+++ src/edu/sju/ee98/daq/core/frequency/response/FrequencyResponse.java
@@ -17,39 +17,28 @@ import org.apache.commons.math3.complex.Complex;
  *
  * @author 102m05008
  */
-public class FrequencyResponse implements Serializable {
+public class FrequencyResponse {
 
-    private String generateChannel;
-    private String responseChannel;
-    private double voltage;
-//
-    private double minFrequency;
-    private double maxFrequrncy;
-    private double baseFrequency;
-    private int length;
+    private FrequencyResponseConfig config;
     //
+    private double baseFrequency;
     private int generateLength = 1000;
 
-    public FrequencyResponse(String generateChannel, String responseChannel, double voltage, double minFrequency, double maxFrequrncy, int length) {
-        this.generateChannel = generateChannel;
-        this.responseChannel = responseChannel;
-        this.voltage = voltage;
+    public FrequencyResponse(FrequencyResponseConfig config) {
+        this.config = config;
 
-        this.minFrequency = Math.log10(minFrequency);
-        this.maxFrequrncy = Math.log10(maxFrequrncy);
-        this.length = length;
-        this.baseFrequency = (this.maxFrequrncy - this.minFrequency) / length;
+        this.baseFrequency = (this.config.getMaxFrequrncy() - this.config.getMinFrequency()) / this.config.getLength();
     }
 
     private double getFrequency(int step) {
-        return Math.pow(10, minFrequency + (baseFrequency * step));
+        return Math.pow(10, this.config.getMinFrequency() + (baseFrequency * step));
     }
 
     public DAQmx createGenerate(double frequency) throws Exception {
-        WaveGenerator analogGenerator = new WaveGenerator(frequency * 20, generateLength, this.voltage, frequency);
+        WaveGenerator analogGenerator = new WaveGenerator(frequency * 20, generateLength, this.config.getVoltage(), frequency);
         DAQmx generate = new DAQmx();
         generate.createTask("");
-        generate.createAOVoltageChan(generateChannel, "", -10, 10, DAQmx.Val_Volts, null);
+        generate.createAOVoltageChan(this.config.getGenerateChannel(), "", -10, 10, DAQmx.Val_Volts, null);
         generate.cfgSampClkTiming("", frequency * 20, DAQmx.Val_Rising, DAQmx.Val_ContSamps, generateLength);
         generate.writeAnalogF64(generateLength, false, 10.0, DAQmx.Val_GroupByChannel, analogGenerator.getData());
         return generate;
@@ -59,61 +48,62 @@ public class FrequencyResponse implements Serializable {
         return new Response(frequency);
     }
 
-    public int getLength() {
-        return this.length;
-    }
-
+//    public int getLength() {
+//        return this.config.getLength();
+//    }
     @Override
     public String toString() {
-        return "FrequencyResponseConfig{" + "inputChannel=" + responseChannel + ", outputChannel=" + generateChannel + ", voltage=" + voltage + ", minFrequency=" + minFrequency + ", maxFrequrncy=" + maxFrequrncy + ", baseFrequency=" + baseFrequency + ", length=" + length + '}';
+        return "FrequencyResponse{" + "config=" + config + ", baseFrequency=" + baseFrequency + ", generateLength=" + generateLength + '}';
     }
 
-    public Complex[] process() {
-        Complex[] data = new Complex[this.length];
+    public FrequencyResponseFile process() {
+        Complex[] input = new Complex[this.config.getLength()];
+        Complex[] output = new Complex[this.config.getLength()];
         DAQmx generate;
-        FrequencyResponse.Response createResponse;
-        for (int i = 0; i < data.length; i++) {
+        Response response;
+        for (int i = 0; i < this.config.getLength(); i++) {
             try {
                 double frequency = this.getFrequency(i);
                 generate = this.createGenerate(frequency);
                 generate.startTask();
 //                Thread.sleep(10);
-                createResponse = this.createResponse(frequency);
-                Complex H = createResponse.H();
-                data[i] = H;
+                response = this.createResponse(frequency);
+                input[i] = response.input;
+                output[i] = response.output;
                 generate.stopTask();
                 generate.clearTask();
             } catch (Exception ex) {
                 Logger.getLogger(FrequencyResponse.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
-        return data;
+        return new FrequencyResponseFile(config, input, output);
     }
 
     private class Response {
 
-        private DAQmx task = new DAQmx();
-        private double frequency;
+        private final Complex input;
+        private final Complex output;
 
         public Response(double frequency) throws Exception {
-            this.frequency = frequency;
+            DAQmx task = new DAQmx();
             task.createTask("");
-            task.createAIVoltageChan(responseChannel, "", DAQmx.Val_Cfg_Default, -10.0, 10.0, DAQmx.Val_Volts, null);
+            task.createAIVoltageChan(config.getResponseChannel(), "", DAQmx.Val_Cfg_Default, -10.0, 10.0, DAQmx.Val_Volts, null);
             task.cfgSampClkTiming("", frequency * 20, DAQmx.Val_Rising, DAQmx.Val_FiniteSamps, 1024);
             task.startTask();
-        }
-
-        public Complex H() throws Exception {
             double data[] = new double[2048];
             task.readAnalogF64(1024, 10, DAQmx.Val_GroupByChannel, data, data.length, null);
-            Complex in = new Wave(frequency * 20, Arrays.copyOfRange(data, 0, 1024)).getValue(frequency);
-            Complex out = new Wave(frequency * 20, Arrays.copyOfRange(data, 1024, 2048)).getValue(frequency);
-
+            input = new Wave(frequency * 20, Arrays.copyOfRange(data, 0, 1024)).getValue(frequency);
+            output = new Wave(frequency * 20, Arrays.copyOfRange(data, 1024, 2048)).getValue(frequency);
             task.stopTask();
             task.clearTask();
-//            System.out.println(Arrays.toString(data));
-            System.out.println("H=" + out + "/" + out);
-            return out.divide(in);
+        }
+
+        public Complex getInput() {
+            return input;
+        }
+
+        public Complex getOutput() {
+            return output;
         }
 
     }
